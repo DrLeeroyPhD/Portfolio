d16df106afe4dddf9100ee09faa64ccbef04bcf9
Christopher <flaminnutz@gmail.com>
2015-12-02 14:11:34 -0800

    Coming back to project updates
    Becoming reacquainted with the project and making small changes to SEO
    pertaining more accurately defining skillset.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-var nav = document.getElementsByTagName("a");
-for(var i = 0; i < nav.length; i++){
-    console.log(nav[i]);
+var navi = document.getElementsByTagName("a");
+
+navi.mousedown = function(){
+    var startY = window.pageYOffset;
+    console.log(startY);    
+//    var jump = "#" + this.getAttribute("name");
+//    document.getElementsByTagName("html body").scrollTop = jump;
 }
 
 //$("nav ul a").click(function(){
@@ -9,3 +13,37 @@ for(var i = 0; i < nav.length; i++){
 //        scrollTop: $(jump).offset().top
 //    }, 500);
 //});
+
+
+
+// http://web.archive.org/web/20140213105950/http://itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+
+//function smoothScroll(eID) {
+//    var stopY = elmYPosition(eID);
+//    var distance = stopY > startY ? stopY - startY : startY - stopY;
+//    if (distance < 100) {
+//        scrollTo(0, stopY); return;
+//    }
+//    var speed = Math.round(distance / 100);
+//    if (speed >= 20) speed = 20;
+//    var step = Math.round(distance / 25);
+//    var leapY = stopY > startY ? startY + step : startY - step;
+//    var timer = 0;
+//    if (stopY > startY) {
+//        for ( var i=startY; i<stopY; i+=step ) {
+//            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+//            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+//        } return;
+//    }
+//    for ( var i=startY; i>stopY; i-=step ) {
+//        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+//        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+//    }
+//}
+
+
+//Setting top CSS settings according to a:active w/ transition
+
+//Function
+//Get starting Y position and where target position is
+// endPos - startPos = distance
