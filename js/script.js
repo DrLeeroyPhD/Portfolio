fa2793b1bf48ec88399eda971c3f56a82aaae471
Christopher <flaminnutz@gmail.com>
2015-12-02 19:59:18 -0800

    Social Media Icon Rollover Functionality
    Added social media icons and wrote a javascript function to allow them
    to rollover on hover.
    Also removed the unnecessary font link for "Lobster" since I'm now using
    "Orbitron" for the header.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+/* Navigation Scrolling Function */
 $("nav ul a").click(function(){
     var jump = "#" + $(this).attr("name");
     $('html, body').animate({
         scrollTop: $(jump).offset().top
     }, 500);
+});
+
+/* Social Icon Rollover function */
+$("#contactLeft img").hover(function() {
+    var achName = $(this).attr("name");
+    $(this).attr("src", "img/soc/" + achName + "_hover.png");
+			}, function() {
+    var achName = $(this).attr("name");
+    $(this).attr("src", "img/soc/" + achName + ".png");
 });
